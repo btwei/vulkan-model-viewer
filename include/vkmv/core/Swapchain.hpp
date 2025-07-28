b2aae177b78de4ee61b2299a385d8ca8910ebc7b
Ben Wei <ben.stwei@gmail.com>
2025-07-27 23:00:28 -0700

    Add empty Swapchain class

---
+++ include/vkmv/core/Swapchain.hpp
@@ -0,0 +1,23 @@
+// Copyright (c) 2025 Benjamin Wei
+//
+// This file is part of the vulkan-model-viewer project.
+// This code is licensed under the MIT license (see http://opensource.org/licenses/MIT)
+
+#ifndef VKMV_SWAPCHAIN_HPP
+#define VKMV_SWAPCHAIN_HPP
+
+namespace vkmv
+{
+    
+class Swapchain {
+public:
+    static void create();
+    static void destroy();
+private:
+
+};
+
+} // namespace vkmv
+
+
+#endif // VKMV_SWAPCHAIN_HPP
---
+++ src/core/Swapchain.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2025 Benjamin Wei
+//
+// This file is part of the vulkan-model-viewer project.
+// This code is licensed under the MIT license (see http://opensource.org/licenses/MIT)
+
+#include "vkmv/core/Swapchain.hpp"
+
+namespace vkmv {
+    
+
+
+} // namespace vkmv
